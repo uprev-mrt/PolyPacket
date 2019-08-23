a5b6bafb7e36d44c8a997b3162ff85972fcd7a9e
Jason Berger <berge472@gmail.com>
2019-08-23 01:36:08 -0400

    added options

--- polypacket/templates/util_main_template.c
+++ polypacket/templates/util_main_template.c
@@ -14,12 +14,31 @@
 #include <pthread.h>
 #include <readline/readline.h>
 
-
 #include "app_${proto.name.lower()}.h"
+
+#define KNRM  "\033[1;0m"
+#define KRED  "\033[1;31m"
+#define KGRN  "\033[1;32m"
+#define KYEL  "\033[1;33m"
+#define KBLU  "\033[1;34m"
+#define KMAG  "\033[1;35m"
+#define KCYN  "\033[1;36m"
+#define KWHT  "\033[1;37m"
+
 
 void quit();
 void input_handler(char* input);
 void print_usage(const char* messageType);
+
+char **packet_name_completion(const char *, int, int);
+char *packet_name_generator(const char *, int);
+
+char *packet_names[] = {
+%for packet in proto.packets:
+    "${packet.name.lower()}",
+%endfor
+    NULL
+};
 
 
 
@@ -39,12 +58,16 @@ int main(int argc, char* argv[])
   char* input;
   pthread_t thread_id;
 
+  int interface_mode = UART_MODE;
   int baud = 9600;
   char* portName = "/dev/ttyUSB0";
+  char* remoteHost = NULL;
+  uint16_t udpPort = 8020;
+  printf(KGRN);
 
 
   /* Look for options. */
-    while ((opt = getopt(argc, argv, "mb:p:")) != -1)
+    while ((opt = getopt(argc, argv, "mb:p:u:t:")) != -1)
     {
         switch (opt)
         {
@@ -56,6 +79,14 @@ int main(int argc, char* argv[])
             case 'b':
               baud = atoi(optarg);
               break;
+            case 'u':
+              udpPort = atoi(optarg);
+              interface_mode = UDP_MODE;
+              break;
+            case 't':
+              remoteHost = optarg;
+              interface_mode = UDP_MODE;
+              break;
             default:
             {
                 print_usage(NULL);
@@ -65,17 +96,21 @@ int main(int argc, char* argv[])
     }
 
   /* Initialize app/service */
-  app_${proto.name.lower()}_init("/dev/ttyUSB0", 9600);
+  app_${proto.name.lower()}_init(portName, baud);
 
 
   pthread_create(&thread_id, NULL, processThread, NULL);
+  rl_attempted_completion_function = packet_name_completion;
 
   while(1)
   {
       input = readline(">>");
-      add_history(input);
-      input_handler(input);
-      free(input);
+      if(input != NULL)
+      {
+        add_history(input);
+        input_handler(input);
+        free(input);
+      }
   }
 
   quit();
@@ -90,11 +125,6 @@ int main(int argc, char* argv[])
 void input_handler(char* input)
 {
 
-  if(*input!=0)
-  {
-      add_history(input);
-  }
-
   if(input[0] == '{')
   {
       //TODO send json
@@ -102,7 +132,7 @@ void input_handler(char* input)
   }
 
   char* messageType = strtok(input," ");
-  char* messageFields = strtok(NULL,0);
+  char* messageFields = strtok(NULL,"");
   int fieldCount =0;
   bool success = false;
 
@@ -121,7 +151,8 @@ void input_handler(char* input)
   else if(!strcasecmp(messageType, "${packet.name}"))       //${packet.desc}
   {
 %if len(packet.fields) >0:
-    fieldCount = sscanf(messageFields, "\
+    if(messageFields != NULL)
+      fieldCount = sscanf(messageFields, "\
 %for field in packet.fields:
 ${field.getFormat()} \
 %endfor
@@ -147,6 +178,14 @@ ${field.getFormat()} \
   }
 %endif
 %endfor
+  else if(!strcasecmp(messageType, "exit()"))
+  {
+    quit();
+  }
+  else
+  {
+      printf("%scommand not recognized..%s\n",KRED,KGRN);
+  }
 
   if(!success)
   {
@@ -156,7 +195,7 @@ ${field.getFormat()} \
 
 void print_usage(const char* messageType)
 {
-
+    printf(KYEL);
 %for packet in proto.packets:
 %if not packet.standard:
 
@@ -174,6 +213,32 @@ void print_usage(const char* messageType)
   }
   %endif
 %endfor
+    printf(KGRN);
+}
+
+char** packet_name_completion(const char *text, int start, int end)
+{
+    rl_attempted_completion_over = 1;
+    return rl_completion_matches(text, packet_name_generator);
+}
+
+char* packet_name_generator(const char *text, int state)
+{
+    static int list_index, len;
+    char *name;
+
+    if (!state) {
+        list_index = 0;
+        len = strlen(text);
+    }
+
+    while ((name = packet_names[list_index++])) {
+        if (strncmp(name, text, len) == 0) {
+            return strdup(name);
+        }
+    }
+
+    return NULL;
 }
 
 
