d0a316d96f7a9d3c64cfe917b32a1a98207f0298
Jason Berger <berge472@gmail.com>
2020-08-12 11:20:22 -0400

    updated version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='polypacket',
-     version='1.0.39',
+     version='1.0.40',
      author="Jason Berger",
      author_email="JBerger@up-rev.com",
      description="A tool for building protocol services",
